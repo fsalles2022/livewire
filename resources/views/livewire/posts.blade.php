17659549e387c761215e4afb5eaf8aae78f0258a
fs2050 <fabiolfs76@hotmail.com>
2023-03-20 20:16:11 -0300

    Criado model, migrate e compoents Posts - Integrado TailwindCss ao Projeto

---
+++ resources/views/livewire/posts.blade.php
@@ -0,0 +1,40 @@
+<div>
+    <div>
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="md:col-span-1">
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-base font-semibold leading-6 text-gray-900">Create Post</h3>
+                </div>
+            </div>
+            <div class="mt-5 md:col-span-2 md:mt-0">
+                <form action="#" method="POST">
+                    <div class="shadow sm:overflow-hidden sm:rounded-md">
+                        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                            <div class="grid grid-cols-3 gap-6">
+                                <div class="col-span-3 sm:col-span-2">
+                                    <label for="company-website" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                                    <div class="mt-2 flex rounded-md shadow-sm">
+                                        <input type="text" wire:model="title" class="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="www.example.com">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+                                <div class="mt-2">
+                                    <textarea wire:model="body" rows="3" class="mt-1 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6" placeholder="you@example.com"></textarea>
+                                </div>
+                            </div>
+
+
+                     
+                        </div>
+                        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                            <button type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,14 @@
     <title>Livewire</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     @livewireStyles
+    <link href="{!! asset('css/app.css') !!}" rel="stylesheet">
 </head>
 
 <body>
 
 
-    <div class="container">
-        @livewire('hello-world')
+    <div class="container mx-auto bg-gray-200">
+        <livewire:posts>
     </div>
 
 
